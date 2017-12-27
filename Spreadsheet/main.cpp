b4264b774407740fe3ac62764c4ce65777b98fcb
LeeVicent <1003766125@qq.com>
2017-12-27 11:49:59 +0800

    12-27

--- Spreadsheet/main.cpp
+++ Spreadsheet/main.cpp
@@ -3,13 +3,12 @@
 #include <QApplication>
 #include <QTableWidget>
 
-
 int main(int argc, char *argv[])
 {
 	QApplication a(argc, argv);
 	MainWindow m;
 	m.show();
-
+	
 	Spreadsheet s;
 	s.show();
 	//QTableWidget q;
